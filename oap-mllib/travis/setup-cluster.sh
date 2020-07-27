c953b431ef8e28670d13c92c55ad01d793b38ae4
Xiaochang Wu <xiaochang.wu@intel.com>
2020-07-27 21:20:42 +0800

    Update setup-cluster.sh

--- oap-mllib/travis/setup-cluster.sh
+++ oap-mllib/travis/setup-cluster.sh
@@ -1,5 +1,10 @@
+#!/usr/bin/env bash
 
-./travis/config-ssh.sh
+WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $WORK_DIR
+
+./config-ssh.sh
 
 cd /opt
 wget https://archive.apache.org/dist/spark/spark-3.0.0/spark-3.0.0-bin-hadoop2.7.tgz
@@ -7,11 +12,13 @@ tar -xzf spark-3.0.0-bin-hadoop2.7.tgz
 wget https://archive.apache.org/dist/hadoop/core/hadoop-2.7.7/hadoop-2.7.7.tar.gz
 tar -xzf hadoop-2.7.7.tar.gz
 
-cp ./travis/core-site.xml /opt/hadoop-2.7.7/etc/hadoop/
-cp ./travis/hdfs-site.xml /opt/hadoop-2.7.7/etc/hadoop/
-cp ./travis/yarn-site.xml /opt/hadoop-2.7.7/etc/hadoop/
-cp ./travis/hadoop-env.sh /opt/hadoop-2.7.7/etc/hadoop/
-cp ./travis/spark-defaults.conf /opt/spark-3.0.0-bin-hadoop2.7/conf
+cd $WORK_DIR
+
+cp ./core-site.xml /opt/hadoop-2.7.7/etc/hadoop/
+cp ./hdfs-site.xml /opt/hadoop-2.7.7/etc/hadoop/
+cp ./yarn-site.xml /opt/hadoop-2.7.7/etc/hadoop/
+cp ./hadoop-env.sh /opt/hadoop-2.7.7/etc/hadoop/
+cp ./spark-defaults.conf /opt/spark-3.0.0-bin-hadoop2.7/conf
 
 /opt/hadoop-2.7.7/sbin/stop-yarn.sh
 /opt/hadoop-2.7.7/sbin/stop-dfs.sh
