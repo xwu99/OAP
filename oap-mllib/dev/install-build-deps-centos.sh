717f8f1681e70297a0e8c3470aad461621d5831f
Wu, Xiaochang <xiaochang.wu@intel.com>
2020-08-24 10:50:49 +0800

    Modify install-build-deps-centos, add run-kmeans-pyspark

--- oap-mllib/dev/install-build-deps-centos.sh
+++ oap-mllib/dev/install-build-deps-centos.sh
@@ -12,12 +12,12 @@ repo_gpgcheck=1
 gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
 EOF
 sudo mv /tmp/oneAPI.repo /etc/yum.repos.d
-sudo yum install intel-oneapi-daal-devel-2021.1-beta07 intel-oneapi-tbb-devel-2021.1-beta07
+sudo yum install intel-oneapi-daal-devel-2021.1-beta08 intel-oneapi-tbb-devel-2021.1-beta08
 
 echo "Building oneCCL ..."
 cd /tmp
 git clone https://github.com/oneapi-src/oneCCL
-git checkout -b 2021.1-beta07-1 origin/2021.1-beta07-1
+git checkout -b beta08 origin/beta08
 cd oneCCL && mkdir build && cd build
 cmake ..
 make -j 2 install
@@ -25,8 +25,8 @@ make -j 2 install
 #
 # Setup building environments manually:
 #
-# export ONEAPI_ROOT=/opt/intel/inteloneapi
-# source /opt/intel/inteloneapi/daal/2021.1-beta07/env/vars.sh
-# source /opt/intel/inteloneapi/tbb/2021.1-beta07/env/vars.sh
+# export ONEAPI_ROOT=/opt/intel/oneapi
+# source $ONEAPI_ROOT/daal/2021.1-beta08/env/vars.sh
+# source $ONEAPI_ROOT/tbb/2021.1-beta08/env/vars.sh
 # source /tmp/oneCCL/build/_install/env/setvars.sh
 #
