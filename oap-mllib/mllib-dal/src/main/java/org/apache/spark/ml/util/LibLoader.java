d63a9bf33a7fd253775bcf56971b99f6d88791b2
Wu, Xiaochang <xiaochang.wu@intel.com>
2020-08-28 22:14:56 +0800

    update

--- oap-mllib/mllib-dal/src/main/java/org/apache/spark/ml/util/LibLoader.java
+++ oap-mllib/mllib-dal/src/main/java/org/apache/spark/ml/util/LibLoader.java
@@ -23,6 +23,8 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.intel.daal.utils.LibUtils;
+
 public final class LibLoader {
     private static final String LIBRARY_PATH_IN_JAR = "/lib";
     private final static String subDir = "MLlibDAL_" + new Date().getTime();
@@ -55,6 +57,7 @@ public final class LibLoader {
     }
 
     public static synchronized void loadLibMLlibDAL() throws IOException {
+        LibUtils.loadLibrary();
         loadFromJar(subDir, "libMLlibDAL.so");
     }
 
