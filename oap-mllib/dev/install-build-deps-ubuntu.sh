27e175b1f81d0e7b1a92983a75e362995fea48a5
Wu, Xiaochang <xiaochang.wu@intel.com>
2020-11-03 16:29:49 +0800

    fix ci scripts for oneapi beta10

--- oap-mllib/dev/install-build-deps-ubuntu.sh
+++ oap-mllib/dev/install-build-deps-ubuntu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -f /opt/intel/inteloneapi ]; then
+if [ ! -f /opt/intel/oneapi ]; then
   echo "Installing oneAPI components ..."
   cd /tmp
   wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
@@ -8,7 +8,7 @@ if [ ! -f /opt/intel/inteloneapi ]; then
   rm GPG-PUB-KEY-INTEL-SW-PRODUCTS-2023.PUB
   echo "deb https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
   sudo apt-get update
-  sudo apt-get install intel-oneapi-daal-devel-2021.1-beta07 intel-oneapi-tbb-devel-2021.1-beta07
+  sudo apt-get install intel-oneapi-dal-devel-2021.1-beta10 intel-oneapi-tbb-devel-2021.1-beta10
 else
   echo "oneAPI components already installed!"
 fi  
@@ -24,8 +24,8 @@ make -j 2 install
 #
 # Setup building environments manually:
 #
-# export ONEAPI_ROOT=/opt/intel/inteloneapi
-# source /opt/intel/inteloneapi/daal/2021.1-beta07/env/vars.sh
-# source /opt/intel/inteloneapi/tbb/2021.1-beta07/env/vars.sh
+# export ONEAPI_ROOT=/opt/intel/oneapi
+# source /opt/intel/oneapi/dal/latest/env/vars.sh
+# source /opt/intel/oneapi/tbb/latest/env/vars.sh
 # source /tmp/oneCCL/build/_install/env/setvars.sh
 #
